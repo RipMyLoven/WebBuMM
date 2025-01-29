d005c4b24a4698b8dec93820ea50886a0388967b
ripmyloven <ripmyloven@gmail.com>
2025-01-29 22:56:28 +0200

    front commit

---
+++ frontend/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+
+    <div>
+      <p>None</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { data } = await useFetch("http://localhost:3001/api/hello");
+console.log(data.value);
+
+</script>
